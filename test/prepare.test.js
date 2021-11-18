fa14f5611149cda268ab5ec6921fa71f92cafa87
Tony Brix <tony@brix.ninja>
2021-11-18 16:53:26 -0600

    fix: update deps

--- test/prepare.test.js
+++ test/prepare.test.js
@@ -2,7 +2,7 @@ import path from 'node:path';
 import test from 'ava';
 import fs from 'fs-extra';
 import tempy from 'tempy';
-import execa from 'execa';
+import {execa} from 'execa';
 import sinon from 'sinon';
 import {WritableStreamBuffer} from 'stream-buffers';
 import prepare from '../lib/prepare.js';
